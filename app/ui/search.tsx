dec9c1dd84bef7ef1e471bd556f7a50bc7e42fa3
rosariiodiiogo <rosariiodiiogo@gmail.com>
2025-07-05 21:45:16 +0300

    In your <Search> Component, import a function called useDebouncedCallback

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import {useDebouncedCallback} from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -10,15 +11,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function handleSearch(term: string)
   {
-    console.log(`Searching... ${term}`);
+    const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
 
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString}`);
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString}`);
+  }, 300);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
