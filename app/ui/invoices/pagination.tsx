01ec3163be53a5a20305cb778e9956d383d599f1
rosariiodiiogo <rosariiodiiogo@gmail.com>
2025-07-05 22:09:57 +0300

    create a new function inside the <Pagination> Component called createPageURL

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -14,6 +14,12 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
 
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
