c3d253d109cad26dd6be44b0f890e4f3d69935f2
rosariiodiiogo <rosariiodiiogo@gmail.com>
2025-07-05 22:15:20 +0300

    updating the handleSearch function in your <Search> component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,20 +9,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  function handleSearch(term: string)
-  {
-    const handleSearch = useDebouncedCallback((term) => {
-      console.log(`Searching... ${term}`);
-
+  const handleSearch = useDebouncedCallback((term) => {
       const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
       if (term) {
         params.set('query', term);
       } else {
         params.delete('query');
       }
       replace(`${pathname}?${params.toString}`);
-    }, 300);
-  }
+  }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
